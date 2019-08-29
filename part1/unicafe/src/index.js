b58fc193ff863c13fceff3828a60a354ef165a7c
Andrew Real <andrew@andrewreal.com>
2019-08-29 20:53:06 +0100

    STep 1.6 Buttons working

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,6 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Result = (props) => {
+  return (
+    <p>{props.text} {props.value}</p>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -9,7 +29,14 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text="Give Feedback"/>
+      <Button text="Good" handleClick={()=>setGood(good+1)}/>
+      <Button text="Neutral" handleClick={()=>setNeutral(neutral+1)}/>
+      <Button text="Bad" handleClick={()=>setBad(bad+1)}/>
+      <Header text="Statistics"/>
+      <Result text="Good" value={good}/>
+      <Result text="Neutral" value={neutral}/>
+      <Result text="Bad" value={bad}/>
     </div>
   )
 }
