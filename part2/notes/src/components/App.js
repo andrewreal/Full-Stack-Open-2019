885fdbbe443892d914f11e73dba42562b2e8e029
Andrew Real <andrew@andrewreal.com>
2019-09-23 21:18:07 +0100

    Notes - Save notes to server

--- part2/notes/src/components/App.js
+++ part2/notes/src/components/App.js
@@ -36,8 +36,13 @@ const App = (props) => {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() > 0.5,
-      id: notes.length + 1,
     }
+    axios
+      .post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response);
+        
+      })
     setNotes(notes.concat(noteObject))
     setNewNote('')
   }
